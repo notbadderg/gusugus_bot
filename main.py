4f5ee8a633762b11977b00abe4c88e3dab68b6ab
notbadderg <notbadderg@gmail.com>
2024-01-23 13:25:15 +0500

    discord bot ok

--- main.py
+++ main.py
@@ -15,21 +15,20 @@ def main():
     cfg = get_cfg()
     msgs = CustomMessages(cfg['TWITCH_URL'], cfg['YOUTUBE_URL'])
 
-
-    # Test
-    # tg_bot = TelegramBot(cfg['TG_API_ROOT'], cfg['TG_TOKEN'], cfg['TG_ALLOWED_USERS'], cfg['TG_CHANNEL_ID'])
-    # response = tg_bot.send_msg(msg.stream_everywhere_string())
-    # print(f'{response.status_code}: {response.text}')
-
     message = msgs.stream_starts_soon() + msgs.stream_everywhere_string()
 
+    # Test
+    tg_bot = TelegramBot(cfg['TG_API_ROOT'], cfg['TG_TOKEN'], cfg['TG_ALLOWED_USERS'], cfg['TG_CHANNEL_ID'], msgs)
+    response = tg_bot.send_msg(message)
+    print(f'{response.status_code}: {response.text}')
+
     ds_bot = DiscordBot(cfg['DS_API_ROOT'], cfg['DS_TOKEN'], cfg['DS_CHANNEL_ID'], msgs)
-    # response = ds_bot.send_msg(message)
-    # print(f'{response.status_code}: {response.text}')
+    response = ds_bot.send_msg(message)
+    print(f'{response.status_code}: {response.text}')
 
     # response = ds_bot.get_msgs()
     # print(json.dumps(response.json(), indent=4))
-    print(ds_bot.finish_announce())
+    # print(ds_bot.finish_announce())
 
 
 if __name__ == '__main__':
--- src/tg_bot.py
+++ src/tg_bot.py
@@ -2,11 +2,12 @@ import requests
 
 
 class TelegramBot:
-    def __init__(self, api_root, token, allowed_users, channel_id):
+    def __init__(self, api_root, token, allowed_users, channel_id, msgs):
         self.api_root = api_root
         self.token = token
         self.allowed_users = allowed_users
         self.channel_id = channel_id
+        self.msgs = msgs
 
         self.request_root = self.api_root + self.token
 
